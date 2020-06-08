7cef7de982d0c4e558eb8f68e8621e3798b82298
Huluvu424242 <huluvu424242@gmail.com>
2020-06-08 21:14:12 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,3 +1,5 @@
-function getSlideShowURL(){
+function createScriptTag(){
   document.write("<script type=\"module\" src=\"https://unpkg.com/@huluvu424242/honey-slideshow@0.0.1/dist/honey-slideshow/honey-slideshow.js\"> </script>");        
 }
+
+createScriptTag();
