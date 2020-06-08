ec13ada7681ebc51cd6a71b29de32bd6e7e55515
Huluvu424242 <huluvu424242@gmail.com>
2020-06-08 21:23:46 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
-function createScriptTag(){
-  window.document.writeln("<script type=\"module\" src=\"https://unpkg.com/@huluvu424242/honey-slideshow@0.0.1/dist/honey-slideshow/honey-slideshow.js\"> </script>");        
-}
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://unpkg.com/@huluvu424242/honey-slideshow@0.0.1/dist/honey-slideshow/honey-slideshow.js';    
 
-createScriptTag();
+document.head.appendChild(script);
+
