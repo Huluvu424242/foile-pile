ffe3035bfd5ccaff817278ac289e7e562fd35857
Huluvu424242 <huluvu424242@gmail.com>
2020-06-27 15:00:15 +0200

    webcomponent upgrade to 0.0.3

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://unpkg.com/@huluvu424242/honey-slideshow@0.0.2/dist/honey-slideshow/honey-slideshow.js';    
+script.src = 'https://unpkg.com/@huluvu424242/honey-slideshow@0.0.3/dist/honey-slideshow/honey-slideshow.js';    
 
 document.head.appendChild(script);
 
