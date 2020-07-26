04237ec69852c5606cdc77019c35dacf2ab67218
Huluvu424242 <huluvu424242@gmail.com>
2020-07-26 14:58:56 +0200

    version 0.0.6 used

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://unpkg.com/@huluvu424242/honey-slideshow@0.0.5/dist/honey-slideshow/honey-slideshow.js';    
+script.src = 'https://unpkg.com/@huluvu424242/honey-slideshow@0.0.6/dist/honey-slideshow/honey-slideshow.js';    
 
 document.head.appendChild(script);
 
